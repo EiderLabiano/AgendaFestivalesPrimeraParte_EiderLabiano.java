6fa3291aa9f7bf1efb99d3c4b882b8b328fa9426
EiderLabiano <elabianbeg@educacion.navarra.es>
2024-02-27 00:12:19 +0100

    Clase modificada y terminada.

--- src/FestivalesIO.java
+++ src/FestivalesIO.java
@@ -41,56 +41,37 @@ public class FestivalesIO {
      */
     public static Festival parsearLinea(String lineaFestival) {
         String nombreDefinitivo = "";
-       String[] miFestival = lineaFestival.trim().split(":");
-       String nombre = "";
-       String lugar = "";
-       LocalDate fechaInicio = null;
-       int duracion = 0;
-       HashSet<Estilo> miEstilo = new HashSet<>();
-       for (int i = 0; i < miFestival.length; i++)
-       {
-           if (i == 0)
-           {
-               int t = 0;
-                nombre = miFestival[i];
-                for (int n = 0; n < nombre.length(); n++)
-                {
-                    if (n == 0)
-                    {
+        String[] miFestival = lineaFestival.split(":");
+        String nombre;
+        String lugar = "";
+        LocalDate fechaInicio = null;
+        int duracion = 0;
+        HashSet<Estilo> miEstilo = new HashSet<>();
+        for (int i = 0; i < miFestival.length; i++) {
+            if (i == 0) {
+                int t = 0;
+                nombre = miFestival[i].trim();
+                for (int n = 0; n < nombre.length(); n++) {
+                    if (n == 0) {
                         nombreDefinitivo = nombreDefinitivo + nombre.toUpperCase().charAt(n);
-                    } else if (nombre.charAt(n) == ' ')
-                    {
+                    } else if (nombre.charAt(n) == ' ') {
                         t = n + 1;
                         nombreDefinitivo = nombreDefinitivo + " " + nombre.toUpperCase().charAt(t);
-                    }
-                    else if (n != t)
-                    {
+                    } else if (n != t) {
                         nombreDefinitivo = nombreDefinitivo + nombre.charAt(n);
                     }
                 }
-           }
-           else if (i == 1)
-           {
-               lugar = miFestival[i].toUpperCase();
-           }
-           else if (i == 2)
-           {
-               fechaInicio = LocalDate.parse(miFestival[i], DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-           }
-           else if (i == 3)
-           {
-               duracion = Integer.parseInt(miFestival[i]);
-           }
-           else
-           {
-               Estilo estilito = Estilo.valueOf(miFestival[i].toUpperCase());
-               miEstilo.add(estilito);
-           }
-       }
-       return new Festival(nombreDefinitivo, lugar, fechaInicio, duracion,  miEstilo);
+            } else if (i == 1) {
+                lugar = miFestival[i].trim().toUpperCase();
+            } else if (i == 2) {
+                fechaInicio = LocalDate.parse(miFestival[i].trim(), DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+            } else if (i == 3) {
+                duracion = Integer.parseInt(miFestival[i].trim());
+            } else {
+                Estilo estilito = Estilo.valueOf(miFestival[i].trim().toUpperCase());
+                miEstilo.add(estilito);
+            }
+        }
+        return new Festival(nombreDefinitivo, lugar, fechaInicio, duracion, miEstilo);
     }
-    
-   
-    
-    
 }
